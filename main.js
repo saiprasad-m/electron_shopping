d1e136abc0b5997ef9c04a1617e3c74b2c51f914
Sai Prasad M <sai.prasad@rocketmail.com>
2018-12-05 14:10:23 +0530

    Add Item, Delete Item, Clear Item, styling

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
 let addWindow;
+let mainMainTemplate = [];
+
+process.env.NODE_ENV = 'production'
 
 
 app.on('ready', () => {
@@ -24,28 +27,40 @@ app.on('ready', () => {
 function createAddWindow() {
     addWindow = new BrowserWindow({
         width: 500,
-        height: 400,
-        title: 'Add Item to Shopping'
+        height: 250,
+        title: 'Add Item to Shopping',
+        frame: false
     });
     addWindow.loadURL(url.format({
         pathname : path.join(__dirname, 'addWindow.html'),
         protocol: 'file:',
         slashes: true
     }))
+    //addWindow.setMenu(null);
 }
 
-const mainMainTemplate = [
+ipcMain.on('item:add', (e,item) => {
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+})
+
+mainMainTemplate = [
     {
         label: 'File',
         submenu: [
             {
                 label: 'Add Item',
+                accelerator: process.platform == 'darwin' ? 'Command+N' : 'Ctrl+N',
                 click() {
                     createAddWindow();
                 }
             },
             {
-                label: 'Clear Item'
+                label: 'Clear Item',
+                accelerator: process.platform == 'darwin' ? 'Command+D' : 'Ctrl+D',
+                click() {
+                    mainWindow.webContents.send('item:clear');
+                }
             },
             {   
                 label: 'Quit',
@@ -57,3 +72,26 @@ const mainMainTemplate = [
         ]
     }
 ]
+
+if(process.platform == 'darwin') {
+    mainMainTemplate.unshift({});
+}
+
+if(process.env.NODE_ENV !== 'production') {
+    mainMainTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle Dev Tools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role : 'reload'
+            }
+        ]
+    })
+
+}
